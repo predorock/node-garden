6a638540f0116cb0840c0179083a48c4be4f9f6b
Lou4 <tagliabue.luca@gmail.com>
2015-12-11 01:51:05 +0100

    reguire field

--- routes/game.js
+++ routes/game.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-
+var field = require('../helpers/field');
 router.get('/game-init', function(req, res, next) {
     field.initGarden();
     res.send("Test create field, check node console");
